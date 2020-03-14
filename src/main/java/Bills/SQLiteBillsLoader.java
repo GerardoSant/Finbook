90590d792c3ffc217c4ea6d263205ea4b0ffee0c
GerardoSant <gbuenclima@gmail.com>
2020-03-14 13:53:06 +0100

    Now SQLiteBillsLoader extends from abstract class SQLiteLoade, remove own parse method and now use DateParser class

--- src/main/java/Bills/SQLiteBillsLoader.java
+++ src/main/java/Bills/SQLiteBillsLoader.java
@@ -1,17 +1,18 @@
 package Bills;
 
+import SQLite.SQLiteLoader;
 import util.DateParser;
-
 import java.sql.*;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-public class SQLiteBillsLoader implements BillsLoader {
+public class SQLiteBillsLoader extends SQLiteLoader implements BillsLoader {
 
+    @Override
+    public List<Bill> loadBills(String RFC) {
+        return selectAll(RFC);
+    }
 
     public List<Bill> selectAll(String RFC){
         RFC="\"" + RFC + "\"";
@@ -29,7 +30,7 @@ public class SQLiteBillsLoader implements BillsLoader {
     private List<Bill> dbBillList(ResultSet resultSet) throws SQLException, ParseException {
         List<Bill> billsList = new ArrayList<>();
         while(resultSet.next()){
-            billsList.add(new Bill(resultSet.getString("UUID"),parseDate(resultSet.getString("Date")),
+            billsList.add(new Bill(resultSet.getString("UUID"),new DateParser("yyyy-MM-dd HH:mm:ss").parseDate(resultSet.getString("Date")),
                     resultSet.getInt("PC"),resultSet.getString("type"),resultSet.getString("use"),resultSet.getString("concept"),
                     resultSet.getString("issuerName"),resultSet.getString("issuerRFC"),resultSet.getString("receiverName"), resultSet.getString("receiverRFC"),
                     resultSet.getDouble("subtotal"),resultSet.getDouble("taxRate"),resultSet.getDouble("total"), resultSet.getString("currency"), resultSet.getString("xml")));
@@ -37,27 +38,4 @@ public class SQLiteBillsLoader implements BillsLoader {
         return billsList;
     }
 
-    private Date parseDate(String date) throws ParseException {
-        return new DateParser("yyyy-MM-dd HH:mm:ss").parseDate(date);
-    }
-
-
-    public Connection connect() {
-        // SQLite connection string
-        //String url = "jdbc:sqlite:C:\\Users\\pezpo\\Desktop\\SQLite\\Bills.db";
-        String url = "jdbc:sqlite:C:\\Users\\pezpo\\Desktop\\SQLite\\facturasSQLite.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-            System.out.println("Connected");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
-
-    @Override
-    public List<Bill> loadBills(String RFC) {
-        return selectAll(RFC);
-    }
 }
