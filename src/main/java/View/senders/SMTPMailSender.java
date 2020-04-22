db6f14bf64847aed7bb38f7875459cdb323e975b
GerardoSant <gbuenclima@gmail.com>
2020-04-22 04:04:33 +0200

    Refactor applied to SMTPMailSender

--- src/main/java/View/senders/SMTPMailSender.java
+++ src/main/java/View/senders/SMTPMailSender.java
@@ -21,56 +21,61 @@ import java.util.Properties;
 
 public class SMTPMailSender implements MailSender{
 
+    private static final String USER = "keyfinderifttt@gmail.com";
+    private static final String PASSWORD= "keyfinder1234";
+
     @Override
     public void sendBillByMail(String mailTo, Bill bill, User from) throws MessagingException, IOException {
-        final String user="keyfinderifttt@gmail.com";
-        final String password="keyfinder1234";
-        String msg = "You have received a bill from: + " + from.getCompanyName() + "\n";
+        try {
+            sendEmail(mailTo, bill, from);
+        } catch (Exception e){
+            System.out.println("Invalid address");
+            throw e;
+        }
+
+    }
+
+    private void sendEmail(String mailTo, Bill bill, User from) throws IOException, MessagingException {
+        Transport.send(message(mailTo, from, billXMLFile(bill)));
+    }
+
+    private MimeMessage message(String mailTo, User from, File billXMLFile) throws MessagingException {
+        MimeMessage message = new MimeMessage(session());
+        message.setFrom(new InternetAddress(USER));
+        message.addRecipient(Message.RecipientType.TO,new InternetAddress(mailTo));
+        message.setSubject("FinBook Notification - You have received a bill from " + from.getCompanyName());
+        DataSource source = new FileDataSource(billXMLFile);
+        message.setDataHandler(new DataHandler(source));
+        message.setFileName(billXMLFile.getName());
+        return message;
+    }
+
+    private File billXMLFile(Bill bill) throws IOException {
+        File billXMLFile = new File(bill.getUUID() + ".xml");
+        billXMLFile.createNewFile();
+        Path path = Paths.get(billXMLFile.getPath());
+        BufferedWriter writer = Files.newBufferedWriter(path);
+        writer.write(bill.getXmlFile());
+        writer.close();
+        return billXMLFile;
+    }
 
 
+    private Session session() {
+        return Session.getDefaultInstance(properties(),
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(USER,PASSWORD);
+                        }
+                    });
+    }
+
+    private Properties properties() {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
         props.put("mail.smtp.port", "587"); //TLS Port
         props.put("mail.smtp.auth", "true"); //enable authentication
         props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
-
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(user,password);
-                    }
-                });
-
-        //Compose the message
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(user));
-            message.addRecipient(Message.RecipientType.TO,new InternetAddress(mailTo));
-            message.setSubject("FinBook Notification - You have received a bill from " + from.getCompanyName());
-            File file1 = new File(bill.getUUID() + ".xml");
-            file1.createNewFile();
-            Path path = Paths.get(file1.getPath());
-            BufferedWriter writer = Files.newBufferedWriter(path);
-            writer.write(bill.getXmlFile());
-            writer.close();
-            DataSource source = new FileDataSource(file1);
-            message.setDataHandler(new DataHandler(source));
-            message.setFileName(file1.getName());
-
-            //send the message
-            Transport.send(message);
-            file1.delete();
-
-            System.out.println("Message sent successfully!");
-        } catch (SendFailedException e){
-            System.out.println("Invalid Address");
-            throw e;
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            throw e;
-        } catch(Exception e){
-            throw e;
-        }
-
+        return props;
     }
 }
